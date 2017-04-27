10c714c78d176cb74d0ad9c39f9061dc9a4c0d8d
Anveshak <anveshak@Anveshaks-iMac.local>
2017-04-27 05:07:00 -0700

    Initial Commit

---
+++ ReadMore2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReadMore2
+//
+//  Created by Anveshak on 27/04/17.
+//  Copyright © 2017 Anveshak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ReadMore2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ReadMore2
+//
+//  Created by Anveshak on 27/04/17.
+//  Copyright © 2017 Anveshak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
